9921b1bef6975a4508a11db92ed6352688163368
JBo1995 <j.m.boyle@hotmail.com>
2017-10-28 16:55:19 +0000

    Version 2.1

---
+++ TestFolder/config.php
@@ -0,0 +1,22 @@
+<?php
+/*
+// mysql_connect("database-host", "username", "password")
+$conn = mysql_connect("localhost","root","root") 
+            or die("cannot connected");
+ 
+// mysql_select_db("database-name", "connection-link-identifier")
+@mysql_select_db("test",$conn);
+*/
+ 
+/**
+ * mysql_connect is deprecated
+ * using mysqli_connect instead
+ */
+ 
+$databaseHost = '127.0.0.1';
+$databaseName = 'test';
+$databaseUsername = 'jboyle';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
