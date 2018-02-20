9f83092c0d5212b4ce95699564a2d853cdef2d4b
James B <j.m.boyle@hotmail.com>
2018-02-20 11:55:47 +0000

    Version 7.5

--- ProductionGrounds/OfficialTemplate/index.php
+++ ProductionGrounds/OfficialTemplate/index.php
@@ -1,39 +1,161 @@
-<?php
-//including the database connection file
-include_once("config.php");
- 
-//fetching data in descending order (lastest entry first)
-//$result = mysql_query("SELECT * FROM CustomerList ORDER BY id DESC"); // mysql_query is deprecated
-$result = mysqli_query($mysqli, "SELECT * FROM Customers"); // using mysqli_query instead
-?>
- 
-<html>
-<head>    
-    <title>Homepage</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<!-- Design by https://dcrazed.com/css-html-login-form-templates/ -->
+<style>
+body{
+  font-family: 'Open Sans', sans-serif;
+  
+   
+   background-color: #cccccc;
+  margin: 0 auto 0 auto;  
+  width:100%; 
+  text-align:center;
+  margin: 20px 0px 20px 0px;   
+}
+
+p{
+  font-size:12px;
+  text-decoration: none;
+  color:#ffffff;
+}
+
+h1{
+  font-size:1.5em;
+  color:#525252;
+}
+
+.box{
+  background:white;
+  width:300px;
+  border-radius:6px;
+  margin: 0 auto 0 auto;
+  padding:0px 0px 70px 0px;
+  border: #2980b9 4px solid; 
+}
+
+.email{
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  border-bottom: #ccc 2px solid;
+  padding: 8px;
+  width:250px;
+  color:#AAAAAA;
+  margin-top:10px;
+  font-size:1em;
+  border-radius:4px;
+}
+
+.password{
+  border-radius:4px;
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  padding: 8px;
+  width:250px;
+  font-size:1em;
+}
+
+.btn{
+  background:#2ecc71;
+  width:125px;
+  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #27ae60 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  float:left;
+  margin-left:16px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+.btn:hover{
+  background:#2CC06B; 
+}
+
+#btn2{
+  float:center;
+  background:#3498db;
+  width:125px;  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #2980b9 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  margin-left:10px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+#btn2:hover{ 
+background:#3594D2; 
+}</style>
+<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>DevLink</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Paper Dashboard core CSS    -->
+    <link href="assets/css/paper-dashboard.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet" />
+
+    <!--  Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Muli:400,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/themify-icons.css" rel="stylesheet">
 </head>
- 
 <body>
-    <a href="add.html">Add New Data</a><br/><br/>
- 
-    <table width='100%' border=0>
-        <tr bgcolor='#CCCCCC'>
-            <td>Name</td>
-            <td>Contact</td>
-            <td>Tasks</td>
-            <td>Paid</td>
-            <td>Update</td>
-        </tr>
-        <?php 
-     //  while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-       while($res = mysqli_fetch_array($result)) {         
-            echo "<tr>";
-            echo "<td>".$res['CustName']."</td>";
-            echo "<td>".$res['CustContact']."</td>";
-            echo "<td>".$res['TaskNum']."</td>";  
-            echo "<td>".$res['Paid']."</td>";  
-            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
-        }
-        ?>
-    </table>
+ <div><h2>Welcome to DevLink</h2><br><h4>The CRM for developers, and the customers who make them.</h4></div>
+<br><br><br>
+
+
+
+
+<br>
+<br>
+<h1><a href="AdminLogin.php">I'm an Admin</a></h1><br><br>
+<h1><a  href="DeveloperLogin.php">I'm a Developer </a></h1><br><br>
+<h1><a  href="CustomerLogin.php">I'm a Customer </a></h1><br><br>
+
+  
+
+  
+
+
+
+  
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+
+
+
+						
+			</article>								
+	</main>
+      </div>
+    </div>
+   
+  </div>
+  
 </body>
 </html>
