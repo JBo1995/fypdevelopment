ce7612f8d12deee417b4d8aa702db8a3ab7391a8
James B <j.m.boyle@hotmail.com>
2018-03-08 10:49:30 +0000

    Version 8.6

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
+	$hostname='127.0.0.1';
+	$user = 'jboyle';
+	$password = '';
+	$mysql_database = 'customersdb';
+	$db = mysql_connect($hostname, $user, $password,$mysql_database);
+	mysql_select_db("customersdb", $db);
+?>
+
+
+<?php
 /*
 // mysql_connect("database-host", "username", "password")
 $conn = mysql_connect("localhost","root","root") 
