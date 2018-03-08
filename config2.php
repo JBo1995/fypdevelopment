5cc17681bd5d79276a7937a924a19f2237b355ff
James B <j.m.boyle@hotmail.com>
2018-03-08 13:02:06 +0000

    Version 8.8

--- config2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$hostname='127.0.0.1';
-	$user = 'jboyle';
-	$password = '';
-	$mysql_database = 'customersdb';
-	$db = mysql_connect($hostname, $user, $password,$mysql_database);
-	mysql_select_db("customersdb", $db);
-?>
